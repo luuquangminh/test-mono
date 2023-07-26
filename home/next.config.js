1ace8d285baafa832218faf7ae2573b345266e70
luuquangminh <minh.luu@cake.vn>
2023-07-26 10:36:08 +0700

    chore:update

--- home/next.config.js
+++ home/next.config.js
@@ -1,24 +1,25 @@
-const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
 
 module.exports = {
   webpack(config, options) {
     if (!options.isServer) {
       config.plugins.push(
         new NextFederationPlugin({
-          name: 'home',
-          filename: 'static/chunks/remoteEntry.js',
+          name: "home",
+          filename: "static/chunks/remoteEntry.js",
           remotes: {
-            home: 'home@http://localhost:3001/_next/static/chunks/remoteEntry.js',
-            shop: 'shop@http://localhost:3002/_next/static/chunks/remoteEntry.js',
-            checkout: 'checkout@http://localhost:3000/_next/static/chunks/remoteEntry.js',
+            home: "home@http://localhost:3001/_next/static/chunks/remoteEntry.js",
+            shop: "shop@https://test-mono-checkout-git-main-luuquangminh.vercel.app/_next/static/chunks//remoteEntry.js",
+            checkout:
+              "checkout@http://localhost:3000/_next/static/chunks/remoteEntry.js",
           },
           exposes: {
-            './nav': './components/nav.js',
-            './home': './realPages/index.js',
-            './pages-map': './pages-map.js',
+            "./nav": "./components/nav.js",
+            "./home": "./realPages/index.js",
+            "./pages-map": "./pages-map.js",
           },
           shared: {},
-        }),
+        })
       );
     }
 
